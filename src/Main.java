79f947b0ba010a709571a7d82530b01a185fa0eb
Renatta Schunemann <renattaschunemann@hotmail.com>
2025-04-17 14:53:22 -0300

    tarefa modulo 09

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        resultadoProva();
     }
+
+    public static double calculomedia() {
+
+        double nota1 = 7;
+        double nota2 = 5.5;
+        double nota3 = 4.5;
+        double nota4 = 4.7;
+        double media;
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        return media;
+
+
+    }
+
+    public static double resultadoProva() {
+        double media = calculomedia();
+
+        if (media >= 6) {
+            System.out.println("Aprovado");
+
+        } else if (media >= 4 && media < 6) {
+            System.out.println("Recuperação");
+
+        } else {
+            System.out.println("Reprovado");
+        }
+
+        System.out.printf("%.2f", media);
+        return media;
+    }
+
+
 }
